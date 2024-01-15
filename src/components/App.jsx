6d3f89ebf274345ff99f818228fefd775f047b12
OxS90 <oxana.slivinschi@gmail.com>
2024-01-15 23:35:18 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import user from './profile';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+import Statistics from './statistics/statistics';
+import Profile from './profile/profile';
+import FriendList from './friends/friends';
+import TransactionHistory from './transactions/transactions';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +16,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
